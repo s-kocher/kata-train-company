4280a9f5e1ca4d6542fcdf4e20644b35eac79bef
Sylvain K <skocher@avb202.com>
2021-05-19 15:28:21 +0200

    Rename stationEnd to stationStop (better consistency)

--- src/main/java/fr/arolla/skocher/traincompany/Trip.java
+++ src/main/java/fr/arolla/skocher/traincompany/Trip.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class Trip {
 
     private final Station stationStart;
-    private final Station stationEnd;
+    private final Station stationStop;
     private final int startedJourneyAt;
 
-    public Trip(Station stationStart, Station stationEnd, int startedJourneyAt) {
+    public Trip(Station stationStart, Station stationStop, int startedJourneyAt) {
         this.stationStart = stationStart;
-        this.stationEnd = stationEnd;
+        this.stationStop = stationStop;
         this.startedJourneyAt = startedJourneyAt;
     }
 
@@ -19,19 +19,19 @@ public class Trip {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Trip trip = (Trip) o;
-        return startedJourneyAt == trip.startedJourneyAt && stationStart == trip.stationStart && stationEnd == trip.stationEnd;
+        return startedJourneyAt == trip.startedJourneyAt && stationStart == trip.stationStart && stationStop == trip.stationStop;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(stationStart, stationEnd, startedJourneyAt);
+        return Objects.hash(stationStart, stationStop, startedJourneyAt);
     }
 
     @Override
     public String toString() {
         return "Trip{" +
             "stationStart='" + stationStart + '\'' +
-            ", stationEnd='" + stationEnd + '\'' +
+            ", stationEnd='" + stationStop + '\'' +
             ", startedJourneyAt=" + startedJourneyAt +
             '}';
     }
